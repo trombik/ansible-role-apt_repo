8c8938eaf2d0dcc2e8bb9068ec824e1cee087a2e
Tomoyuki Sakurai <tomoyukis@reallyenglish.com>
2017-06-16 11:06:11 +0700

    [bugfix][feature] Remove warnings from ansible and support ubuntu 16.04 (#5)
    * QA
    
    * QA rubocopfy
    
    * make ansible happy
    
    * add .github
    
    * support ubuntu 16.04
    
    * make the test release-agnostic
    
    * bailout when os[:release] does not match known releases
    
    * QA
    
    * obtain codename fomr the host, not hard-coding
    
    * QA

--- tests/serverspec/default_spec.rb
+++ tests/serverspec/default_spec.rb
@@ -1,12 +1,15 @@
-require 'spec_helper'
-require 'serverspec'
+require "spec_helper"
+require "serverspec"
+
+codename = Specinfra.backend.run_command("lsb_release -c")
+                    .stdout.strip.split(" ")[1]
 
 describe package("apt-transport-https") do
   it { should be_installed }
 end
 
 describe command("apt-key list") do
-  its(:stdout) { should match(/^pub\s+2048R\/D88E42B4\s+.*$/) }
+  its(:stdout) { should match(%r{^pub\s+2048R/D88E42B4\s+.*$}) }
 end
 
 describe file("/etc/apt/sources.list.d/artifacts_elastic_co_packages_5_x_apt.list") do
@@ -15,7 +18,7 @@ end
 
 case os[:family]
 when "ubuntu"
-  describe file("/etc/apt/sources.list.d/ppa_webupd8team_java_trusty.list") do
-    its(:content) { should match(/^deb #{ Regexp.escape("http://ppa.launchpad.net/webupd8team/java/ubuntu") } trusty main$/) }
+  describe file("/etc/apt/sources.list.d/ppa_webupd8team_java_#{codename}.list") do
+    its(:content) { should match(/^deb #{ Regexp.escape("http://ppa.launchpad.net/webupd8team/java/ubuntu") } #{codename} main$/) }
   end
 end
