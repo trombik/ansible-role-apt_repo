50b9eaab60b32d383f6ee3396ac26c74fb9c1b7e
Tomoyuki Sakurai <y@trombik.org>
2019-05-24 15:52:20 +0900

    feature: support Ubuntu 18.04

--- tests/serverspec/default_spec.rb
+++ tests/serverspec/default_spec.rb
@@ -3,13 +3,31 @@ require "serverspec"
 
 codename = Specinfra.backend.run_command("lsb_release -c")
                     .stdout.strip.split(" ")[1]
+# apt-key output:
+#
+# before 18.04
+# pub   2048R/D88E42B4 2013-09-16
+# uid                  Elasticsearch (Elasticsearch Signing Key) <dev_ops@elasticsearch.org>
+# sub   2048R/60D31954 2013-09-16
+elasticsearch_key_id = "2048R/D88E42B4"
+
+# after 18.04
+# pub   rsa2048 2013-09-16 [SC]
+#      4609 5ACC 8548 582C 1A26  99A9 D27D 666C D88E 42B4
+# uid           [ unknown] Elasticsearch (Elasticsearch Signing Key) <dev_ops@elasticsearch.org>
+# sub   rsa2048 2013-09-16 [E]
+elasticsearch_key = "4609 5ACC 8548 582C 1A26  99A9 D27D 666C D88E 42B4"
 
 describe package("apt-transport-https") do
   it { should be_installed }
 end
 
 describe command("apt-key list") do
-  its(:stdout) { should match(%r{^pub\s+2048R/D88E42B4\s+.*$}) }
+  if os[:release].to_f >= 18.04
+    its(:stdout) { should match(/#{elasticsearch_key}/) }
+  else
+    its(:stdout) { should match(/^pub\s+#{elasticsearch_key_id}\s+.*$/) }
+  end
 end
 
 describe file("/etc/apt/sources.list.d/artifacts_elastic_co_packages_5_x_apt.list") do
